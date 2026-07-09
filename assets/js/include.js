d16820c0d57ce0c722156298c71c806c7be04673
“Nitasubedi” <nitusubedi000@gmail.com>
2026-07-09 13:18:53 +0545

    feature added: navbar,  hero, about

---
+++ assets/js/include.js
@@ -0,0 +1,12 @@
+async function loadComponent(id, file) {
+  const response = await fetch(file);
+
+  const html = await response.text();
+
+  document.getElementById(id).innerHTML = html;
+}
+
+loadComponent("navbar", "components/Navbar.html");
+loadComponent("hero", "components/Hero.html");
+loadComponent("about", "components/About.html");
+loadComponent("news", "components/News.html");
