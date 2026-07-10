3aaf2f5658e6086e07a03a1602ffef8f39be156c
“Nitasubedi” <nitusubedi000@gmail.com>
2026-07-10 13:03:48 +0545

    added about page

--- assets/js/include.js
+++ assets/js/include.js
@@ -6,6 +6,22 @@ async function loadComponent(id, file) {
   document.getElementById(id).innerHTML = html;
 }
 
+document.addEventListener("DOMContentLoaded", () => {
+  const currentPage = window.location.pathname.split("/").pop();
+
+  const navLinks = document.querySelectorAll(".nav-links a");
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+
+    const href = link.getAttribute("href");
+
+    if (href === currentPage || (currentPage === "" && href === "index.html")) {
+      link.classList.add("active");
+    }
+  });
+});
+
 loadComponent("navbar", "components/Navbar.html");
 loadComponent("hero", "components/Hero.html");
 loadComponent("about", "components/About.html");
